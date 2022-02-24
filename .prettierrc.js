c5d8596b9e0f2092207c5ee76a92a7fbdb06781e
Hellden-fixe <r.chapotin@gmail.com>
2022-02-24 18:22:37 +0100

    Init starter template

---
+++ .prettierrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  quoteProps: "as-needed",
+  bracketSameLine: true,
+};
+
+// {
+//   arrowParens: always,
+//   bracketSpacing: true,
+//    bracketSameLine: false,
+//   embeddedLanguageFormatting: auto,
+//   htmlWhitespaceSensitivity: css,
+//   insertPragma: false,
+//   jsxBracketSameLine: false,
+//   jsxSingleQuote: false,
+//   proseWrap: preserve,
+//   requirePragma: false,
+//   semi: true,
+//   singleQuote: false,
+//   tabWidth: 2,
+//   trailingComma: es5,
+//   useTabs: false,
+//   vueIndentScriptAndStyle: false,
+//   printWidth: 100
+// }
